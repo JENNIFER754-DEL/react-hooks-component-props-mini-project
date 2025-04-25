c5159aa2a6837d760aa3ef8947b685a4c2e72381
JENNIFER754-DEL <jennifernyaguthii23@gmail.com>
2025-04-25 10:51:36 +0300

    correct submission

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,15 @@
 import React from "react";
 import blogData from "../data/blog";
 import Header from "./Header";
+import About from "./About";
 import ArticleList from "./ArticleList";
-
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
-}
-
-function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
-    </div>
-  );
-}
-
-function About() {
-  return (
-    <div id="about">
-      <h2>About</h2>
-    </div>
-  );
-}
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <Home />
-      <About />
+      <Header name={blogData.name} />
+      <About image={blogData.image} about={blogData.about} />
+      <ArticleList posts={blogData.posts} />
     </div>
   );
 }
